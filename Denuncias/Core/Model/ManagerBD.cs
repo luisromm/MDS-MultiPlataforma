309f9a84d5aeca60ad187825f04b48180cdcbf0c
Jose Manuel Hernandez Reyes <joss_036@hotmail.com>
2018-11-21 16:32:44 -0600

    ok

--- Denuncias/Core/Model/ManagerBD.cs
+++ Denuncias/Core/Model/ManagerBD.cs
@@ -22,4 +22,8 @@ public class ManagerBD
     {
         strCadenaConexion = ConfigurationManager.ConnectionStrings[strNombreConexion].ConnectionString;
     }
+
+    public void Test() {
+
+    }
 }
