68d10e9403ffc2d1354d6c3eb5035d5a08f40d08
Luis Roberto Morales <tm2_megatron@hotmail.con>
2018-11-21 18:33:33 -0600

    Controller folder added!

---
+++ Denuncias/Core/Controller/ControllerBD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Controller
+{
+    class ControllerBD
+    {
+        //fields
+        private string connectionDB;
+
+        //methods
+        public ControllerBD()
+        {
+            connectionDB = string.Empty;
+        }
+    }
+}
